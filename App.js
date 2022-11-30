d5054c9f021349671fa3c2c25b63ed0408f4b077
LaffSteven <steve.laff@gmail.com>
2022-11-30 10:42:24 -0500

    text test

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// View -> UIView / AndroidView
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is some text that I added myself!</Text>
+      <Text>I think thta using ReactNative is going to be interesting and fun. I think thta using ReactNative is going to be interesting and fun. This is a longer section of text to test out word-wrap!</Text>
       <StatusBar style="auto" />
     </View>
   );
